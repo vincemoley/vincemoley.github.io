b8c5bd24794c1a3954afcadf9285df4119c94b0c
Vince Moley <vincemoley@yahoo.com>
2018-02-16 13:56:32 -0500

    add survey, which points to new domain

--- js/survey.js
+++ js/survey.js
@@ -1,3 +1,11 @@
 $(function(){
-	
+	$(".submit-sign-up-btn").on('click', function(e) {
+		e.preventDefault();
+
+		var form = $(".contact-form");
+
+		$.post("https://app.kleancierge.com/signup", form.serialize(), function(){
+			$("#contact-form").html("<h4 class='text-center alert alert-success'>Thank you for signing up!<br/><br/>A Kleancierge representative will be in touch</h4>");
+		});
+	});
 });
