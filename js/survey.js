83f2a138750e1fe94718904e014f8a15b95d0494
Vince Moley <vincemoley@yahoo.com>
2018-06-12 08:51:41 -0400

    update sign up

--- js/survey.js
+++ js/survey.js
@@ -2,7 +2,35 @@ $(function(){
 	$(".submit-sign-up-btn").on('click', function(e) {
 		e.preventDefault();
 
+		var hasError = false;
 		var form = $(".contact-form");
+		var fName = form.find("[name='firstName']");
+		var lName = form.find("[name='lastName']");
+		var email = form.find("[name='email']");
+		var address = form.find("[name='address']");
+		
+		if(fName.val() === ''){
+			fName.addClass("has-error");
+			hasError = true;
+		}
+		if(lName.val() === ''){
+			lName.addClass("has-error");
+			hasError = true;
+		}
+		if(email.val() === ''){
+			email.addClass("has-error");
+			hasError = true;
+		}
+		if(address.val() === ''){
+			address.addClass("has-error");
+			hasError = true;
+		}
+
+		if(hasError){
+			alert('Please enter the required fields highlighted above');
+
+			return;
+		}
 
 		$.ajax({
 			url: "https://app.kleancierge.com/signup",
