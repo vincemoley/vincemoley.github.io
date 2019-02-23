92a208fc9ba76a49061fab82dd79426911af98fc
Vince Moley <vincemoley@yahoo.com>
2019-02-23 08:17:30 -0500

    update landing page

--- js/main.js
+++ js/main.js
@@ -94,6 +94,17 @@
 	    }
 	});
 
+	$('#about-slider').owlCarousel({
+		items:1,
+		loop:true,
+		margin:15,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		dots : true,
+		autoplay : true,
+		animateOut: 'fadeOut'
+	});
+
 	setTimeout(function(){
 		$('#after-photos').owlCarousel({
 			items:1,
